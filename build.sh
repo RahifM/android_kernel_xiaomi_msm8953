1220953c4bd1328e9162d216da8580a94258b542
RahifM <faizel326@gmail.com>
2020-07-13 12:48:44 +0530

    add build logging
    Signed-off-by: RahifM <faizel326@gmail.com>

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ make O=out ARCH=arm64 mido_defconfig
 PATH="$HOME/android/pa/prebuilts/gcc/linux-x86/aarch64/aarch64-elf/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CROSS_COMPILE=aarch64-elf-
+                      CROSS_COMPILE=aarch64-elf- \
+                      2>&1 | tee buildlog.txt
